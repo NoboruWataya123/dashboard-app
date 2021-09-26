3a6c10ba8e91b13917c63783e02aba12cd3cd044
NoboruWataya123 <jinxw666@gmail.com>
2021-09-26 19:58:42 +0900

    feat: almost done, added all main features

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,31 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view />
+  <component :is="layout" />
+  <p>{{ $t("message.hello", { msg: "hello" }) }}</p>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import AuthLayout from "@/components/AuthLayout";
+import DashboardLayout from "@/components/DashboardLayout";
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+export default {
+  components: {
+    AuthLayout,
+    DashboardLayout,
+  },
+  data() {
+    return {
+      layout: null,
+    };
+  },
+  watch: {
+    $route(to) {
+      // set layout by route meta
+      if (to.meta.layout !== undefined) {
+        this.layout = to.meta.layout;
+      } else {
+        this.layout = "DashboardLayout"; // this is default layout if route meta is not set
+      }
+    },
+  },
+};
+</script>
---
+++ src/views/BankAccount.vue
@@ -0,0 +1,84 @@
+<template>
+  <main id="main" class="has-sidebar">
+    <section class="section dashboard-section">
+      <header class="header">
+        <h1>Bank List</h1>
+        <a href="#addBankAccount" class="btn btn-primary card-popup-opener"
+          >+ Add Bank Account</a
+        >
+      </header>
+      <div class="table-holder">
+        <table class="custom-table">
+          <thead>
+            <tr>
+              <th class="large-cell">Bank</th>
+              <th class="text-center">Type</th>
+              <th class="text-center">Bank No.</th>
+              <th class="text-center">Bank Book Status</th>
+              <th class="btn-cell">Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td data-title="Bank">
+                Bank of Ayudhya Public Company <br />Limited (BAY)
+              </td>
+              <td class="text-center" data-title="Type">Personal</td>
+              <td class="text-center" data-title="Bank No.">
+                1188272778199171
+              </td>
+              <td class="text-center" data-title="Bank Book Status">
+                <span class="status approved">Approve</span>
+              </td>
+              <td data-title="Delete">
+                <a href="#" data-bs-toggle="modal" data-bs-target="#modalDelete"
+                  ><i class="icon-trash"></i
+                ></a>
+              </td>
+            </tr>
+            <tr>
+              <td data-title="Bank">
+                Bank of Ayudhya Public Company <br />Limited (BAY)
+              </td>
+              <td class="text-center" data-title="Type">Personal</td>
+              <td class="text-center" data-title="Bank No.">
+                1188272778199171
+              </td>
+              <td class="text-center" data-title="Bank Book Status">
+                <span class="status approved">Approve</span>
+              </td>
+              <td data-title="Delete">
+                <a href="#" data-bs-toggle="modal" data-bs-target="#modalDelete"
+                  ><i class="icon-trash"></i
+                ></a>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </section>
+  </main>
+</template>
+
+<i18n>
+{
+  "en": {
+    "Bank Account": "Bank Account",
+    "Bank List": "Bank List",
+    "Personal": "Personal",
+    "Approve": "Approve",
+    "Delete": "Delete",
+    "Delete Bank Account": "Delete Bank Account",
+    "Yes": "Yes",
+    "No": "No",
+    "Cancel": "Cancel",
+    "Add Bank Account": "Add Bank Account",
+    "Bank": "Bank",
+    "Type": "Type",
+    "Bank No.": "Bank No.",
+    "Bank Book Status": "Bank Book Status"
+  },
+  "th": {
+  }
+}
+</i18n>
