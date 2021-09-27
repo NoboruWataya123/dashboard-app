6e77178dc3adb220cfd7568c1fe11b1c9a34176a
NoboruWataya123 <jinxw666@gmail.com>
2021-09-27 15:22:34 +0900

    feat: add translation for webhooks

--- src/views/Webhooks.vue
+++ src/views/Webhooks.vue
@@ -2,25 +2,25 @@
   <main id="main" class="has-sidebar">
     <section class="section dashboard-section">
       <header class="header">
-        <h1>Webhooks and API Keys</h1>
+        <h1>{{ $t("Webhooks and API Keys") }}</h1>
         <a
           href="#addWebhook"
           @click="openAddWebhookModal"
           class="btn btn-primary card-popup-opener"
-          >+ Add Webhooks</a
+          >+ {{ $t("Add Webhooks") }}</a
         >
       </header>
       <div class="table-holder">
         <table class="custom-table">
           <thead>
             <tr>
-              <th class="id-cell">ID</th>
-              <th class="large-cell">DOMAIN</th>
-              <th class="large-cell">DOMAIN WEBSITE UPDATE</th>
-              <th>CREATE DATE</th>
-              <th class="btn-cell">View</th>
-              <th class="btn-cell">Edit</th>
-              <th class="btn-cell">Delete</th>
+              <th class="id-cell">{{ $t("ID") }}</th>
+              <th class="large-cell">{{ $t("DOMAIN") }}</th>
+              <th class="large-cell">{{ $t("DOMAIN WEBSITE UPDATE") }}</th>
+              <th>{{ $t("CREATE DATE") }}</th>
+              <th class="btn-cell">{{ $t("View") }}</th>
+              <th class="btn-cell">{{ $t("Edit") }}</th>
+              <th class="btn-cell">{{ $t("Delete") }}</th>
             </tr>
           </thead>
           <tbody>
@@ -87,12 +87,12 @@
     >
       <div class="scroll-area">
         <a href="#" @click="closeAddWebhookModal" class="btn-close"
-          ><span class="sr-only">close</span></a
+          ><span class="sr-only">{{ $t("close") }}</span></a
         >
         <div class="card-body">
-          <h2>Add Webhook</h2>
+          <h2>{{ $t("Add Webhook") }}</h2>
           <div class="form-group">
-            <label for="applicationType">Application Type:</label>
+            <label for="applicationType">{{ $t("Application Type") }}:</label>
             <select id="applicationType" class="form-select">
               <option value="Web Application">Web Application</option>
               <option value="Web Application">Web Application</option>
@@ -100,7 +100,7 @@
             </select>
           </div>
           <div class="form-group">
-            <label for="domain">Domain:</label>
+            <label for="domain">{{ $t("Domain") }}:</label>
             <input
               id="domain"
               type="text"
@@ -109,7 +109,7 @@
             />
           </div>
           <div class="form-group">
-            <label for="webhook">web Hook:</label>
+            <label for="webhook">{{ $t("web Hook") }}:</label>
             <input
               id="webhook"
               type="text"
@@ -126,10 +126,14 @@
                 type="button"
                 class="btn popup-close"
               >
-                Cancel
+                {{ $t("Cancel") }}
               </button>
             </li>
-            <li><button type="submit" class="btn btn-primary">Save</button></li>
+            <li>
+              <button type="submit" class="btn btn-primary">
+                {{ $t("Save") }}
+              </button>
+            </li>
           </ul>
         </div>
       </div>
@@ -141,38 +145,41 @@
     >
       <div class="scroll-area">
         <a href="#" class="btn-close" @click="closeDetailWebhookModal"
-          ><span class="sr-only">close</span></a
+          ><span class="sr-only">{{ $t("close") }}</span></a
         >
         <div class="card-body">
-          <h2>Webhook Detail</h2>
+          <h2>{{ $t("Webhook Detail") }}</h2>
           <ul class="detail-list">
             <li>
-              <strong class="heading">Application Type</strong>
-              <strong class="value">Web Application</strong>
+              <strong class="heading">{{ $t("Application Type") }}</strong>
+              <strong class="value">{{ $t("Web Application") }}</strong>
             </li>
             <li>
-              <strong class="heading">Domain</strong>
+              <strong class="heading">{{ $t("Domain") }}</strong>
               <strong class="value">https://worldwallet.net</strong>
             </li>
             <li>
-              <strong class="heading">Web Hook:</strong>
+              <strong class="heading">{{ $t("Web Hook") }}:</strong>
               <strong class="value">https://worldwallet.net/webhook.php</strong>
             </li>
             <li>
-              <strong class="heading">Secret ID</strong>
+              <strong class="heading">{{ $t("Secret ID") }}</strong>
               <strong class="value">WP54W4W0198P8HEGCAA3</strong>
             </li>
             <li>
-              <strong class="heading">Secret Key </strong>
+              <strong class="heading">{{ $t("Secret Key") }} </strong>
               <strong class="value"
                 >HnxToTosQTT3a9UQoSUiW4hzCZ5OLvb0wJfPr5tcle3fsmklZvd3ADIXTRCziQlATvZviqTpftUG0luN</strong
               >
             </li>
             <li>
-              <strong class="heading">Terms and Conditions</strong>
+              <strong class="heading">{{ $t("Terms and Conditions") }}</strong>
               <strong class="value"
-                >I acknowledge that I will NOT claim the refund or cancel the
-                order.</strong
+                >{{
+                  $t(
+                    "I acknowledge that I will NOT claim the refund or cancel the order"
+                  )
+                }}.</strong
               >
             </li>
           </ul>
@@ -185,7 +192,7 @@
                 type="button"
                 class="btn popup-close"
               >
-                Cancel
+                {{ $t("Cancel") }}
               </button>
             </li>
             <li>
@@ -194,7 +201,7 @@
                 type="submit"
                 class="btn btn-primary"
               >
-                Save
+                {{ $t("Save") }}
               </button>
             </li>
           </ul>
@@ -211,9 +218,9 @@
           ><span class="sr-only">close</span></a
         >
         <div class="card-body">
-          <h2>Webhook Detail</h2>
+          <h2>{{ $t("Webhook Detail") }}</h2>
           <div class="form-group">
-            <label for="applicationType2">Application Type:</label>
+            <label for="applicationType2">{{ $t("Application Type") }}:</label>
             <select id="applicationType2" class="form-select">
               <option value="Web Application">Web Application</option>
               <option value="Web Application">Web Application</option>
@@ -222,35 +229,40 @@
           </div>
           <ul class="detail-list">
             <li>
-              <strong class="heading">Domain</strong>
+              <strong class="heading">{{ $t("Domain") }}</strong>
               <strong class="value">https://worldwallet.net</strong>
             </li>
             <li>
-              <strong class="heading">Web Hook:</strong>
+              <strong class="heading">{{ $t("Web Hook") }}:</strong>
               <strong class="value">https://worldwallet.net/webhook.php</strong>
             </li>
             <li>
-              <strong class="heading">Secret ID</strong>
+              <strong class="heading">{{ $t("Secret ID") }}</strong>
               <strong class="value">WP54W4W0198P8HEGCAA3</strong>
             </li>
             <li>
-              <strong class="heading">Secret Key </strong>
+              <strong class="heading">{{ $t("Secret Key") }} </strong>
               <strong class="value mb-2"
                 >HnxToTosQTT3a9UQoSUiW4hzCZ5OLvb0wJfPr5tcle3fsmklZvd3ADIXTRCziQlATvZviqTpftUG0luN</strong
               >
               <button type="button" class="btn btn-primary">
-                <i class="icon-repeat"></i>Regenerate API Key
+                <i class="icon-repeat"></i>{{ $t("Regenerate API Key") }}
               </button>
             </li>
             <li>
               <div class="form-group mb-0">
-                <label for="termsConditions">Terms and Conditions</label>
+                <label for="termsConditions">{{
+                  $t("Terms and Conditions")
+                }}</label>
                 <select id="termsConditions" class="form-select">
                   <option
                     value="I acknowledge that I will NOT claim the refund or cancel the order."
                   >
-                    I acknowledge that I will NOT claim the refund or cancel the
-                    order.
+                    {{
+                      $t(
+                        "I acknowledge that I will NOT claim the refund or cancel the order"
+                      )
+                    }}.
                   </option>
                   <option value="Option 2">Option 2</option>
                   <option value="Option 3">Option 3</option>
@@ -267,7 +279,7 @@
                 type="button"
                 class="btn popup-close"
               >
-                Cancel
+                {{ $t("Cancel") }}
               </button>
             </li>
             <li>
@@ -276,7 +288,7 @@
                 type="submit"
                 class="btn btn-primary"
               >
-                Save
+                {{ $t("Save") }}
               </button>
             </li>
           </ul>
@@ -300,18 +312,20 @@
               data-bs-dismiss="modal"
               aria-label="Close"
             ></button>
-            <h5 class="h1">Delete</h5>
-            <p>Are you sure you want to delete?</p>
+            <h5 class="h1">{{ $t("Delete") }}</h5>
+            <p>{{ $t("Are you sure you want to delete") }}?</p>
           </div>
           <div class="modal-footer">
             <ul class="buttons-inline-list">
               <li>
                 <button type="button" class="btn" data-bs-dismiss="modal">
-                  Cancel
+                  {{ $t("Cancel") }}
                 </button>
               </li>
               <li>
-                <button type="submit" class="btn btn-primary">yes</button>
+                <button type="submit" class="btn btn-primary">
+                  {{ $t("yes") }}
+                </button>
               </li>
             </ul>
           </div>
@@ -374,3 +388,73 @@ export default {
   line-height: 1.2 !important;
 }
 </style>
+
+<i18n>
+{
+  "en": {
+    "Webhooks and API Keys": "Webhooks and API Keys",
+    "Webhooks_detail": "Webhooks Detail",
+    "Add Webhooks": "Add Webhooks",
+    "Edit Webhooks": "Edit Webhooks",
+    "Webhooks": "Webhooks",
+    "ID": "ID",
+    "DOMAIN": "Domain",
+    "Domain": "Domain",
+    "DOMAIN WEBSITE UPDATE": "Domain Website Update",
+    "CREATE DATE": "Create Date",
+    "Web hook": "Webhook",
+    "Web Hook": "Web Hook",
+    "web Hook": "web Hook",
+    "Secret ID": "Secret ID",
+    "Secret Key": "Secret Key",
+    "Terms and Conditions": "Terms and Conditions",
+    "Regenerate API Key": "Regenerate API Key",
+    "Delete": "Delete",
+    "Cancel": "Cancel",
+    "yes": "yes",
+    "Yes": "Yes",
+    "View": "view",
+    "Edit": "Edit",
+    "Add": "Add",
+    "Save": "Save",
+    "Delete": "Delete",
+    "Application Type": "Application Type",
+    "Web Application": "Web Application",
+    "Webhooks_detail": "Webhooks Detail",
+    "Are you sure you want to delete?": "Are you sure you want to delete?",
+    "I acknowledge that I will NOT claim the refund or cancel the order": "I acknowledge that I will NOT claim the refund or cancel the order"
+  },
+  "th": {
+    "Webhooks and API Keys": "เว็บฮุคและคีย์ API",
+    "Webhooks detail": "รายละเอียดเว็บฮุค",
+    "Add Webhooks": "เพิ่มเว็บฮุค",
+    "Edit Webhooks": "แก้ไขเว็บฮุค",
+    "Webhooks": "เว็บฮุค",
+    "ID": "ID",
+    "DOMAIN": "โดเมน",
+    "Domain": "โดเมน",
+    "DOMAIN WEBSITE UPDATE": "การอัปเดตเว็บไซต์โดเมน",
+    "CREATE DATE": "วันที่สร้าง",
+    "Webhook": "เว็บฮุค",
+    "Web Hook": "เว็บฮุค",
+    "web Hook": "เว็บฮุค",
+    "Secret ID": "ความลับ ID",
+    "Secret Key": "รหัสลับ",
+    "Terms and Conditions": "ข้อกำหนดและเงื่อนไข",
+    "Regenerate API Key": "สร้างคีย์ API ใหม่",
+    "Delete": "ลบ",
+    "Cancel": "ยกเลิก",
+    "yes": "ใช่",
+    "Yes": "ใช่",
+    "View": "ดู",
+    "Edit": "แก้ไข",
+    "Add": "เพิ่ม",
+    "Application Type": "ประเภทการสมัคร",
+    "Save": "บันทึก",
+    "Web Application": "เว็บแอปพลิเคชัน",
+    "Webhooks_detail": "รายละเอียดเว็บฮุค",
+    "Are you sure you want to delete": "คุณแน่ใจหรือว่าต้องการลบ?",
+    "I acknowledge that I will NOT claim the refund or cancel the order": "ฉันรับทราบว่าฉันจะไม่ขอรับเงินคืนหรือยกเลิกคำสั่งซื้อ"
+  },
+}
+</i18n>
